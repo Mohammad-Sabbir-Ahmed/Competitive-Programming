2bf6997a5f279463d0a397ca9da7a9f0b3b05e01
Sabbir Ahmed <sabbirahmed213262@gmail.com>
2020-03-31 01:47:38 +0600

    Prime Factorization using Sieve O(log n) for multiple queries.cpp added

--- Number Theory/Prime Factorization using Sieve O(log n) for multiple queries.cpp
+++ Number Theory/Prime Factorization using Sieve O(log n) for multiple queries.cpp
@@ -15,16 +15,13 @@
 
 using namespace std;
 
-
 #define MAXN 100001
-
 
 int spf[MAXN];
 
 void sieve(){
 
     spf[1] = 1;                             // initialize 1 as a prime
-
     for(int i=2; i<=MAXN; i++)              // initialize every number from 2 to MAXN as prime
         spf[i] = i;
 
@@ -32,18 +29,14 @@ void sieve(){
         spf[i] = 2;
 
     int limit = sqrt(MAXN);
-
     for(int i=3; i<=limit; i++){
-
         if(spf[i] == i){                    // is i is prime then
-
             for(int j=2*i; j<=MAXN; j+=i){  // find all numbers divided by j ans assign j as there least prime factor
                 if (spf[j]==j)              // if j is not marked yet then
                     spf[j] = i;
             }
         }
     }
-
 }
 
 
@@ -62,20 +55,16 @@ int main(){
 //    freopen("in.txt","r",stdin);
 //    freopen("output.txt","w",stdout);
 
-
     sieve();  // find smallest prime factor for every number from 1 to MAXN
 
     int n = 12246;
 
     cout << "Prime factors of " << n << " are: ";
-
     vector <int> answer = findPrimeFactorsOf(n); // find prime factors of n
-
 
     for(int i=0; i<answer.size(); i++)  // print prime factors of n
         cout << answer[i] << " ";
     cout << endl;
-
 
     return 0;
 }
